6d509db8208f4e90b806b5f97374a88e44f1ad3d
Fatma Çadır <fatmacadirr1@gmail.com>
2026-05-03 21:05:54 +0300

    Update movie.js

--- netlify/movie.js
+++ netlify/movie.js
@@ -1,5 +1,5 @@
 exports.handler = async function (event) {
-  const { query, tmdbId, imdbId, lang } = event.queryStringParameters;
+  const { query, tmdbId, tmdbDetailId, imdbId, lang } = event.queryStringParameters;
 
   const OMDB_KEY = process.env.OMDB_KEY;
   const TMDB_KEY = process.env.TMDB_KEY;
@@ -8,8 +8,11 @@ exports.handler = async function (event) {
     let url = "";
 
     if (query) {
-      url = `https://api.themoviedb.org/3/search/movie?api_key=${TMDB_KEY}&query=${encodeURIComponent(query)}&language=${lang || "tr-TR"}`;    } else if (tmdbId) {
+      url = `https://api.themoviedb.org/3/search/movie?api_key=${TMDB_KEY}&query=${encodeURIComponent(query)}&language=${lang || "tr-TR"}`;
+    } else if (tmdbId) {
       url = `https://api.themoviedb.org/3/movie/${tmdbId}/external_ids?api_key=${TMDB_KEY}`;
+    } else if (tmdbDetailId) {
+      url = `https://api.themoviedb.org/3/movie/${tmdbDetailId}?api_key=${TMDB_KEY}&language=${lang || "tr-TR"}`;
     } else if (imdbId) {
       url = `https://www.omdbapi.com/?i=${imdbId}&plot=short&apikey=${OMDB_KEY}`;
     } else {
