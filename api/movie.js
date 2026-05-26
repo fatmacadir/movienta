67c6ed11274689ec4fbad3b04360e762f18b9129
Fatma Çadır <fatmacadirr1@gmail.com>
2026-05-26 16:48:46 +0300

    Update movie.js

--- api/movie.js
+++ api/movie.js
@@ -1,19 +1,30 @@
 export default async function handler(request, response) {
-  const { query, tmdbId, tmdbDetailId, imdbId, lang, popular, genreId, upcoming } = request.query;
+  const {
+    query,
+    tmdbId,
+    tmdbDetailId,
+    imdbId,
+    lang,
+    popular,
+    genreId,
+    upcoming,
+    similarId
+  } = request.query;
+
   const OMDB_KEY = process.env.OMDB_KEY;
   const TMDB_KEY = process.env.TMDB_KEY;
 
   try {
     let url = "";
-    
+
     if (upcoming) {
       url = `https://api.themoviedb.org/3/movie/upcoming?api_key=${TMDB_KEY}&language=${lang || "tr-TR"}&page=1`;
-    }else if (popular) {
+    } else if (popular) {
       const page = request.query.page || 1;
       url = `https://api.themoviedb.org/3/movie/popular?api_key=${TMDB_KEY}&language=${lang || "tr-TR"}&page=${page}`;
-    }else if (genreId) {
-      url = `https://api.themoviedb.org/3/discover/movie?api_key=${TMDB_KEY}&with_genres=${genreId}`;   
-    }else if (query) {
+    } else if (genreId) {
+      url = `https://api.themoviedb.org/3/discover/movie?api_key=${TMDB_KEY}&with_genres=${genreId}&language=${lang || "tr-TR"}&sort_by=popularity.desc&page=1`;
+    } else if (query) {
       url = `https://api.themoviedb.org/3/search/movie?api_key=${TMDB_KEY}&query=${encodeURIComponent(query)}&language=${lang || "tr-TR"}`;
     } else if (tmdbId) {
       url = `https://api.themoviedb.org/3/movie/${tmdbId}/external_ids?api_key=${TMDB_KEY}`;
@@ -35,4 +46,3 @@ export default async function handler(request, response) {
     return response.status(500).json({ error: "Server error" });
   }
 }
-
