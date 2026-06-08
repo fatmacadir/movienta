40d6bc659de36f70ad959df7d4d44bf9356f04df
Fatma Çadır <fatmacadirr1@gmail.com>
2026-06-08 12:56:14 +0300

    Update translation.js

--- translation.js
+++ translation.js
@@ -1,1 +1,57 @@
+const translations = {
+  "tr-TR": {
+    home: "Ana Sayfa",
+    categories: "Kategoriler",
+    favorites: "Favoriler",
+    about: "Hakkında",
+    popularMovies: "Popüler Filmler",
+    upcomingMovies: "Yakında Çıkacak Filmler",
+    aboutTitle: "Movienta Hakkında",
+    aboutText1:
+      "Movienta, HTML, CSS ve JavaScript kullanılarak geliştirilen bir film keşif platformudur.",
+    aboutText2:
+      "Kullanıcılar film arayabilir, kategorileri keşfedebilir, yakında çıkacak filmleri görebilir ve favorilerine ekleyebilir."
+  },
 
+  "en-US": {
+    home: "Home",
+    categories: "Categories",
+    favorites: "Favorites",
+    about: "About",
+    popularMovies: "Popular Movies",
+    upcomingMovies: "Upcoming Movies",
+    aboutTitle: "About Movienta",
+    aboutText1:
+      "Movienta is a movie discovery platform developed with HTML, CSS and JavaScript.",
+    aboutText2:
+      "Users can search movies, explore categories, discover upcoming movies and save their favorites."
+  },
+
+  "de-DE": {
+    home: "Startseite",
+    categories: "Kategorien",
+    favorites: "Favoriten",
+    about: "Über",
+    popularMovies: "Beliebte Filme",
+    upcomingMovies: "Bevorstehende Filme",
+    aboutTitle: "Über Movienta",
+    aboutText1:
+      "Movienta ist eine Filmplattform, die mit HTML, CSS und JavaScript entwickelt wurde.",
+    aboutText2:
+      "Benutzer können Filme suchen, Kategorien entdecken und Favoriten speichern."
+  },
+
+  "es-ES": {
+    home: "Inicio",
+    categories: "Categorías",
+    favorites: "Favoritos",
+    about: "Acerca de",
+    popularMovies: "Películas populares",
+    upcomingMovies: "Próximos estrenos",
+    aboutTitle: "Acerca de Movienta",
+    aboutText1:
+      "Movienta es una plataforma de descubrimiento de películas desarrollada con HTML, CSS y JavaScript.",
+    aboutText2:
+      "Los usuarios pueden buscar películas, explorar categorías y guardar sus favoritos."
+  }
+};
