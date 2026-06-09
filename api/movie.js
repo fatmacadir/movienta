395c4fd6e96be0b87a5513c982cc14060edd8d3a
Fatma Çadır <fatmacadirr1@gmail.com>
2026-06-09 13:30:45 +0300

    Update movie.js

--- api/movie.js
+++ api/movie.js
@@ -23,7 +23,7 @@ export default async function handler(request, response) {
       const page = request.query.page || 1;
       url = `https://api.themoviedb.org/3/movie/popular?api_key=${TMDB_KEY}&language=${lang || "tr-TR"}&page=${page}`;
     } else if (genreId) {
-      url = `https://api.themoviedb.org/3/discover/movie?api_key=${TMDB_KEY}&with_genres=${genreId}&language=${lang || "tr-TR"}&sort_by=popularity.desc&include_adult=false&page=1`;
+      url = `https://api.themoviedb.org/3/discover/movie?api_key=${TMDB_KEY}&with_genres=${genreId}&language=${lang || "tr-TR"}&sort_by=popularity.desc&include_adult=false&certification_country=US&certification.lte=PG-13&page=1`;
     } else if (query) {
       url = `https://api.themoviedb.org/3/search/movie?api_key=${TMDB_KEY}&query=${encodeURIComponent(query)}&language=${lang || "tr-TR"}`;
     } else if (tmdbId) {
