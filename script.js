892ef0c8c825c3677996a27cb4e9f7dbbcee1092
Fatma Çadır <fatmacadirr1@gmail.com>
2026-06-09 12:32:19 +0300

    Update script.js

--- script.js
+++ script.js
@@ -369,3 +369,13 @@ function scrollToMovies() {
 
 window.scrollToMovies = scrollToMovies;
 
+function showToast(text) {
+  const toast = document.getElementById("toast");
+
+  toast.textContent = text;
+  toast.classList.add("show");
+
+  setTimeout(() => {
+    toast.classList.remove("show");
+  }, 2500);
+}
